47987d5ca83d9cd2ab36da644d4a5fc1b84925c8
Lenny <l.git@familie-hanss.de>
2026-03-13 18:29:11 +0100

    Added reconnect functionality

--- main.py
+++ main.py
@@ -8,22 +8,52 @@ app = QApplication(sys.argv)
 app.setQuitOnLastWindowClosed(False)
 
 socket = QWebSocket()
+pingtime = 1000
+timeouttime = 500
+
 
 def on_ws_connected():
     print("Verbunden!")
+    timer_connection_timeout.start()
+    timer_last_message_recieved.start()
+    timer_reconnect.stop()
 
 def on_ws_disconnected():
     print("Getrennt!")
+    timer_reconnect.start()
 
 def on_ws_message(message):
+    timer_connection_timeout.start()
+    timer_last_message_recieved.start()
+
     if message.startswith("H: "):
         height_mm = int(message[3:])
         height_cm = height_mm / 10
         print(f"Höhe: " + str(height_cm) + "cm")
+    elif message.startswith("Info: "):
+        print(f"Info(recived): " + message)
         
-def send(message):
+def ws_send(message):
     socket.sendTextMessage(message)
     print("WS " + message + " gesendet")
+
+def closesocket():
+    timer_connection_timeout.stop()
+    timer_last_message_recieved.stop()
+    socket.close()
+
+
+timer_last_message_recieved = QTimer()
+timer_last_message_recieved.setInterval(pingtime)
+timer_last_message_recieved.timeout.connect(lambda: ws_send("i"))
+
+timer_connection_timeout = QTimer()
+timer_connection_timeout.setInterval(pingtime + timeouttime)
+timer_connection_timeout.timeout.connect(closesocket)
+
+timer_reconnect = QTimer()
+timer_reconnect.setInterval(20*1000)
+timer_reconnect.timeout.connect(lambda: socket.open(QUrl("ws://192.168.1.17:81")))
 
 socket.connected.connect(on_ws_connected)
 socket.disconnected.connect(on_ws_disconnected)
@@ -35,10 +65,10 @@ tray.setIcon(QIcon.fromTheme("computer"))
 tray.setVisible(True)
 
 menu = QMenu()
-menu.addAction("Preset 1", lambda: send("1"))
-menu.addAction("Preset 2", lambda: send("2"))
-menu.addAction("Preset 3", lambda: send("3"))
-menu.addAction("Preset 4", lambda: send("4"))
+menu.addAction("Preset 1", lambda: ws_send("1"))
+menu.addAction("Preset 2", lambda: ws_send("2"))
+menu.addAction("Preset 3", lambda: ws_send("3"))
+menu.addAction("Preset 4", lambda: ws_send("4"))
 menu.addSeparator()
 menu.addAction("Beenden", app.quit)
 
