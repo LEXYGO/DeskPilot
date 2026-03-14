1df1c27018f6aca8aea152c4a79227aa49652c49
Lenny <l.git@familie-hanss.de>
2026-03-14 20:34:11 +0100

    added config.json

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ MIT
 
 This project uses the following open source libraries:
 - [PySide6](https://wiki.qt.io/Qt_for_Python) by The Qt Company, licensed under LGPL v3
-- [PyQt6 WebSockets](https://doc.qt.io/qt-6/qtwebsockets-index.html) – part of PySide6
+- [platformdirs](https://github.com/platformdirs/platformdirs) by the platformdirs contributors, licensed under MIT
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import sys
+import os
+import json
+from platformdirs import user_data_dir
 from PySide6.QtWidgets import QApplication, QSystemTrayIcon, QMenu
 from PySide6.QtWebSockets import QWebSocket
 from PySide6.QtCore import QUrl, QTimer
@@ -7,27 +10,41 @@ from PySide6.QtGui import QIcon
 app = QApplication(sys.argv)
 app.setQuitOnLastWindowClosed(False)
 
+data_dir = user_data_dir("DeskMate", "DeskMate")
+os.makedirs(data_dir, exist_ok=True)
+config_file = os.path.join(data_dir, "config.json")
+
 socket = QWebSocket()
-pingtime = 1000
-timeouttime = 500
+pingtime = 5000
+timeouttime = 3000
 
-max_height = -1
-min_height = -1
-preset_count = -1
+def load_config():
+    if os.path.exists(config_file):
+        with open(config_file, "r") as f:
+            return json.load(f)
+    return {"max_height": -1, "min_height": -1, "preset_count": -1, "ip": "fffff", "port": "fffff", "p1": "", "p2": "", "p3": "", "p4": "", "p5": "", "p6": "", "p7": "", "p8": "", "p9": ""}
 
+def save_config():
+    with open(config_file, "w") as f:
+        json.dump(config, f)
+
+config = load_config()
+print(config)
 
 def on_ws_connected():
     print("Verbunden!")
     timer_connection_timeout.start()
     timer_last_message_recieved.start()
-    timer_reconnect.stop()
     ws_send("i")
+    tray.setContextMenu(tray_menu_connected)
 
 def on_ws_disconnected():
     print("Getrennt!")
-    timer_reconnect.start()
+    tray.setContextMenu(tray_menu_disconnected)
+    connectButton.setEnabled(True)
 
 def on_ws_message(message):
+    print("Nachricht: " + message)
     global max_height, min_height, preset_count
     timer_connection_timeout.start()
     timer_last_message_recieved.start()
@@ -43,7 +60,7 @@ def on_ws_message(message):
         height_mm = int(parts[3])
 
     height_cm = height_mm / 10
-    height_action.setText(f"Höhe: {height_cm}cm")
+    height_tray_action.setText(f"Height: {height_cm}cm")
         
 def ws_send(message):
     socket.sendTextMessage(message)
@@ -54,6 +71,15 @@ def closesocket():
     timer_last_message_recieved.stop()
     socket.close()
 
+def connect_socket():
+    connectButton.setEnabled(False)
+    print("Trying to connect....")
+    socket.open(QUrl("ws://" + config["ip"] + ":" + config["port"]))
+
+# def enable_systray():
+
+# def disable_systray():
+
 
 timer_last_message_recieved = QTimer()
 timer_last_message_recieved.setInterval(pingtime)
@@ -63,31 +89,48 @@ timer_connection_timeout = QTimer()
 timer_connection_timeout.setInterval(pingtime + timeouttime)
 timer_connection_timeout.timeout.connect(closesocket)
 
-timer_reconnect = QTimer()
-timer_reconnect.setInterval(20*1000)
-timer_reconnect.timeout.connect(lambda: socket.open(QUrl("ws://192.168.1.17:81")))
+
+tray = QSystemTrayIcon()
+tray.setIcon(QIcon.fromTheme("computer"))
+
+tray_menu_connected = QMenu()
+if config["preset_count"] >= 1:
+    tray_menu_connected.addAction("Preset 1" + " --- " + config["p1"], lambda: ws_send("1"))
+if config["preset_count"] >= 2:
+    tray_menu_connected.addAction("Preset 2" + " --- " + config["p2"], lambda: ws_send("2"))
+if config["preset_count"] >= 3:
+    tray_menu_connected.addAction("Preset 3" + " --- " + config["p3"], lambda: ws_send("3"))
+if config["preset_count"] >= 4:
+    tray_menu_connected.addAction("Preset 4" + " --- " + config["p4"], lambda: ws_send("4"))
+if config["preset_count"] >= 5:
+    tray_menu_connected.addAction("Preset 5" + " --- " + config["p5"], lambda: ws_send("5"))
+if config["preset_count"] >= 6:
+    tray_menu_connected.addAction("Preset 6" + " --- " + config["p6"], lambda: ws_send("6"))
+if config["preset_count"] >= 7:
+    tray_menu_connected.addAction("Preset 7" + " --- " + config["p7"], lambda: ws_send("7"))
+if config["preset_count"] >= 8:
+    tray_menu_connected.addAction("Preset 8" + " --- " + config["p8"], lambda: ws_send("8"))
+if config["preset_count"] >= 9:
+    tray_menu_connected.addAction("Preset 9" + " --- " + config["p9"], lambda: ws_send("9"))
+tray_menu_connected.addSeparator()
+tray_menu_connected.addAction("Beenden", app.quit)
+tray_menu_connected.addSeparator()
+height_tray_action = tray_menu_connected.addAction("Höhe: --")
+height_tray_action.setEnabled(False)
+
+tray_menu_disconnected = QMenu()
+tray_menu_disconnected.addAction("Not connected to ESP").setEnabled(False )
+connectButton = tray_menu_disconnected.addAction("CONNECT", lambda: connect_socket())
+tray_menu_disconnected.addSeparator()
+tray_menu_disconnected.addAction("Beenden", app.quit)
+
+tray.setContextMenu(tray_menu_disconnected)
+
+tray.setVisible(True)
 
 socket.connected.connect(on_ws_connected)
 socket.disconnected.connect(on_ws_disconnected)
 socket.textMessageReceived.connect(on_ws_message)
-socket.open(QUrl("ws://192.168.1.17:81"))
-
-tray = QSystemTrayIcon()
-tray.setIcon(QIcon.fromTheme("computer"))
-tray.setVisible(True)
-
-menu = QMenu()
-menu.addAction("Preset 1", lambda: ws_send("1"))
-menu.addAction("Preset 2", lambda: ws_send("2"))
-menu.addAction("Preset 3", lambda: ws_send("3"))
-menu.addAction("Preset 4", lambda: ws_send("4"))
-menu.addSeparator()
-menu.addAction("Beenden", app.quit)
-menu.addSeparator()
-height_action = menu.addAction("Höhe: --")
-height_action.setEnabled(False)
-
-
-tray.setContextMenu(menu)
+connect_socket()
 
 sys.exit(app.exec())
